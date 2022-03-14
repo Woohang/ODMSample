4ebfdda100877b0e802f35f4d0764ca31d533116
Administrator <Administrator@172.20.194.157>
2022-03-14 17:22:50 +0900

    주석 ODM

--- miniloan-xom/src/miniloan/Borrower.java
+++ miniloan-xom/src/miniloan/Borrower.java
@@ -36,7 +36,7 @@ public class Borrower {
 	// address 추가(2022.03.10) by Woohang
 	private String address;
 	
-	// gender 추가(2022.03.14) by Woohang office(odm 샘플) 주석 머지
+	// gender 추가(2022.03.14) by Woohang odm 정리
 	private String gender;
 
 	public Borrower() {
